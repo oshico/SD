97bff0ec3fc81aa08ee599dc35dff606560b76cd
oshico <2022111404@ufp.edu.pt>
2025-03-27 16:56:18 +0000

    added rabbitmq folder and sub pl01

---
+++ src/edu/ufp/inf/sd/rabbitmqservices/p01_hello/runscripts/setenv.sh
@@ -0,0 +1,63 @@
+#!/usr/bin/env bash
+
+export SCRIPT_ROLE=$1
+
+export JDK=/usr/lib/jvm/java-17-openjdk-amd64
+export INTELLIJ=IntelliJ
+export CURRENT_IDE=${INTELLIJ}
+export USERNAME=$(whoami)
+
+export JAVAPROJ_NAME=SD
+export JAVAPROJ=/home/${USERNAME}/IdeaProjects/${JAVAPROJ_NAME}
+export RABBITMQ_SERVICES_FOLDER=edu/ufp/inf/sd/rabbitmqservices
+export RABBITMQ_SERVICES_PACKAGE=edu.ufp.inf.sd.rabbitmqservices
+export PACKAGE=p01_hello
+export QUEUE_NAME_PREFIX=hello
+export EXCHANGE_NAME_PREFIX=NA
+export PRODUCER_CLASS_PREFIX=Send
+export CONSUMER_CLASS_PREFIX=Recv
+
+#@REM ==== NETWORK STUFF ====
+export BROKER_HOST=localhost
+export BROKER_PORT=5672
+export BROKER_HTTP_PORT=15672
+
+#@REM ======================== DO NOT CHANGE AFTER THIS POINT ========================
+export JAVAPACKAGE=${RABBITMQ_SERVICES_PACKAGE}.${PACKAGE}
+export JAVAPACKAGEROLE=${JAVAPACKAGE}.${SCRIPT_ROLE}
+export JAVAPACKAGEROLEPATH=${RABBITMQ_SERVICES_FOLDER}/${PACKAGE}/${SCRIPT_ROLE}
+export JAVASCRIPTSPATH=${RABBITMQ_SERVICES_FOLDER}/${PACKAGE}/runscripts
+export BROKER_QUEUE=${QUEUE_NAME_PREFIX}_queue
+export BROKER_EXCHANGE=${EXCHANGE_NAME_PREFIX}_exchange
+export SERVICE_URL=http://${BROKER_HOST}:${BROKER_PORT}
+
+export PATH=${PATH}:${JDK}/bin
+
+if [[ "${CURRENT_IDE}" == "${NETBEANS}" ]]; then
+    export JAVAPROJ_SRC=src
+    export JAVAPROJ_CLASSES=build/classes/
+    export JAVAPROJ_DIST=dist
+    export JAVAPROJ_DIST_LIB=lib
+elif [[ "${CURRENT_IDE}" == "${INTELLIJ}" ]]; then
+    export JAVAPROJ_SRC=src
+    export JAVAPROJ_CLASSES=out/production/${JAVAPROJ_NAME}/
+    export JAVAPROJ_DIST=out/artifacts/${JAVAPROJ_NAME}
+    export JAVAPROJ_DIST_LIB=lib
+fi
+
+export JAVAPROJ_CLASSES_FOLDER=${JAVAPROJ}/${JAVAPROJ_CLASSES}
+export JAVAPROJ_JAR_FILE=${JAVAPROJ}/${JAVAPROJ_DIST}/${JAVAPROJ_NAME}.jar
+export JAVA_LIB_FOLDER=${JAVAPROJ}/${JAVAPROJ_DIST_LIB}
+export JAVA_RABBITMQ_TOOLS=${JAVA_LIB_FOLDER}/amqp-client-5.9.0.jar:${JAVA_LIB_FOLDER}/slf4j-api-1.7.30.jar:${JAVA_LIB_FOLDER}/slf4j-simple-1.7.30.jar
+
+#export CLASSPATH=${JAVAPROJ_CLASSES_FOLDER}:${JAVA_RABBITMQ_TOOLS}
+#export CLASSPATH=${JAVAPROJ_JAR_FILE}
+export CLASSPATH=${JAVAPROJ_JAR_FILE}:${JAVA_RABBITMQ_TOOLS}
+
+export ABSPATH2CLASSES=${JAVAPROJ_CLASSES_FOLDER}
+export ABSPATH2SRC=${JAVAPROJ}/${JAVAPROJ_SRC}
+
+
+
+
+
