e8e95c84a1f8a8484bdcfdd6128938a7b914cede
oshico <2022111404@ufp.edu.pt>
2025-05-23 22:42:08 +0100

    implmented the functions to update , create , and delete files/folders

--- src/edu/ufp/inf/sd/project/client/ClientMain.java
+++ src/edu/ufp/inf/sd/project/client/ClientMain.java
@@ -1,26 +1,15 @@
 package edu.ufp.inf.sd.project.client;
 
 import edu.ufp.inf.sd.project.server.AuthServiceRI;
-import edu.ufp.inf.sd.project.server.SessionRI;
-import edu.ufp.inf.sd.project.server.SubjectFileSystemRI;
 import edu.ufp.inf.sd.project.util.SetupContextRMI;
 
-import java.awt.*;
 import java.rmi.NotBoundException;
 import java.rmi.RemoteException;
 import java.rmi.registry.Registry;
-import java.util.List;
-import java.util.Timer;
-import java.util.TimerTask;
-import java.util.Scanner;
-
 
 public class ClientMain {
     private Registry registry;
     private AuthServiceRI authService;
-    private SessionRI session;
-    private SubjectFileSystemRI fileSystem;
-    private ObserverRI observer;
 
     public static void main(String[] args) {
         if (args.length < 3) {
@@ -54,124 +43,10 @@ public class ClientMain {
 
             System.out.println("Found service " + serviceName + " at " + registryHost + ":" + registryPort);
 
-            runConsole();
 
         } catch (RemoteException | NotBoundException e) {
             System.err.println("Error connecting to server: " + e.getMessage());
             System.exit(1);
         }
     }
-
-    private void runConsole() {
-        try (Scanner scanner = new Scanner(System.in)) {
-            boolean loggedIn = false;
-
-            while (!loggedIn) {
-                System.out.println("1. Register");
-                System.out.println("2. Login");
-                System.out.print("Choose option: ");
-                String choice = scanner.nextLine();
-
-                System.out.print("Username: ");
-                String username = scanner.nextLine();
-                System.out.print("Password: ");
-                String password = scanner.nextLine();
-
-                switch (choice) {
-                    case "1":
-                        if (authService.register(username, password)) {
-                            System.out.println("Registered successfully!");
-                        } else {
-                            System.out.println("Username already exists.");
-                        }
-                        break;
-                    case "2":
-                        session = authService.login(username, password);
-                        if (session != null) {
-                            observer =
-                            System.out.println("Logged in successfully.");
-                            loggedIn = true;
-                        } else {
-                            System.out.println("Invalid credentials.");
-                        }
-                        break;
-                    default:
-                        System.out.println("Invalid option.");
-                }
-            }
-
-            commandLoop(scanner);
-
-        } catch (RemoteException e) {
-            System.err.println("Remote error: " + e.getMessage());
-        }
-    }
-
-    private void commandLoop(Scanner scanner) throws RemoteException {
-        while (true) {
-            System.out.println("\nAvailable commands:");
-            System.out.println("1. Create Document");
-            System.out.println("2. Open Document");
-            System.out.println("3. Edit Document");
-            System.out.println("4. Switch Folder");
-            System.out.println("5. Share Folder");
-            System.out.println("6. Exit");
-            System.out.print("Enter command: ");
-
-            String input = scanner.nextLine();
-
-            switch (input) {
-                case "1":
-                    System.out.print("Document name: ");
-                    String newDoc = scanner.nextLine();
-                    fileSystem.createDocument(newDoc);
-                    System.out.println("Created document: " + newDoc);
-                    break;
-                case "2":
-                    System.out.print("Document name: ");
-                    String docToOpen = scanner.nextLine();
-                    currentDocument = docToOpen;
-                    String content = fileSystem.openDocument(docToOpen);
-                    System.out.println("Opened document:\n" + content);
-                    break;
-                case "3":
-                    if (currentDocument == null) {
-                        System.out.println("No document open.");
-                        break;
-                    }
-                    System.out.println("Enter new content (end with an empty line):");
-                    StringBuilder sb = new StringBuilder();
-                    String line;
-                    while (!(line = scanner.nextLine()).isEmpty()) {
-                        sb.append(line).append("\n");
-                    }
-                    lastEditedContent = sb.toString();
-                    fileSystem.updateDocument(currentDocument, lastEditedContent, currentUsername);
-
-                    break;
-                case "4":
-                    System.out.print("Folder name: ");
-                    String folderToSwitch = scanner.nextLine();
-                    fileSystem.attachObserver(folderToSwitch, observer);
-                    System.out.println("Switched to folder .");
-                    break;
-                case "5":
-                    System.out.print("Folder name: ");
-                    String folder = scanner.nextLine();
-                    System.out.print("Username to share with: ");
-                    String shareTo = scanner.nextLine();
-                    fileSystem.shareFolder(folder, shareTo);
-                    System.out.println("Shared folder " + folder + " with " + shareTo);
-                    break;
-                case "6":
-                    System.out.println("Exiting.");
-                    fileSystem.updateDocument(currentDocument, lastEditedContent, currentUsername);
-                    System.exit(0);
-                default:
-                    System.out.println("Unknown command.");
-            }
-        }
-    }
-
-
 }
--- src/edu/ufp/inf/sd/project/server/StateFileSystemOperation.java
+++ src/edu/ufp/inf/sd/project/server/StateFileSystemOperation.java
@@ -1,8 +1,6 @@
 package edu.ufp.inf.sd.project.server;
 
-import java.io.Serial;
-import java.io.Serializable;
-import java.time.LocalDateTime;
+import java.io.*;
 import java.util.ArrayList;
 
 public class StateFileSystemOperation implements Serializable {
@@ -28,16 +26,96 @@ public class StateFileSystemOperation implements Serializable {
         this.type = type;
     }
 
-   public static void executeOperation(StateFileSystemOperation sfso) {
+    public static void executeOperation(StateFileSystemOperation sfso) {
         switch (sfso.type) {
             case CREATEFILE:
-
+                String filePath = sfso.params.get(0);
+                String fileName = sfso.params.get(1);
+                File dir = new File(filePath);
+                if (!dir.exists()) {
+                    dir.mkdirs();
+                    File file = new File(dir, fileName);
+                    try {
+                        if (file.createNewFile()) {
+                            System.out.println("File " + fileName + " created");
+                        } else {
+                            System.out.println("File " + fileName + " already exists");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                } else {
+                    File file = new File(dir, fileName);
+                    try {
+                        if (file.createNewFile()) {
+                            System.out.println("File " + fileName + " created");
+                        } else {
+                            System.out.println("File " + fileName + " already exists");
+                        }
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
                 break;
             case UPDATEFILE:
+                String filePath2 = sfso.params.get(0);
+                String fileName2 = sfso.params.get(1);
+                String fileContent2 = sfso.params.get(2);
+                File dir2 = new File(filePath2);
+                if (!dir2.exists()) {
+                    System.out.println("File " + filePath2 + " is not at " + dir2.getAbsolutePath());
+                } else {
+                    File file2 = new File(dir2, fileName2);
+                    if (!file2.exists()) {
+                        System.out.println("File " + filePath2 + " does not exist at " + file2.getAbsolutePath());
+                    } else {
+                        try {
+                            FileWriter fw = new FileWriter(file2);
+                            fw.write(fileContent2);
+                            System.out.println("File " + fileName2 + " updated");
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+
+                    }
+                }
 
                 break;
             case DELETEFILE:
+                String filePath3 = sfso.params.get(0);
+                String fileName3 = sfso.params.get(1);
+                File dir3 = new File(filePath3);
+                if (!dir3.exists()) {
+                    System.out.println("File is not at:" + filePath3);
+                } else {
+                    File file3 = new File(dir3, fileName3);
+                    if (file3.delete()) {
+                        System.out.println("File " + fileName3 + " deleted");
+                    } else {
+                        System.out.println("File " + fileName3 + " does not exist");
+                    }
+                }
+                break;
+            case CREATEFOLDER:
+                String dirPath4 = sfso.params.get(0);
+                File dir4 = new File(dirPath4);
+                if (!dir4.exists()) {
+                    dir4.mkdirs();
+                    System.out.println("Folder created: " + dir4.getAbsolutePath());
+                } else {
+                    System.out.println("Folder already exists: " + dir4.getAbsolutePath());
+                }
+                break;
+            case DELETEFOLDER:
+                String dirPath5 = sfso.params.get(0);
+                File dir5 = new File(dirPath5);
+                if (dir5.delete()) {
+                    System.out.println("Folder deleted: " + dirPath5);
+                } else {
+                    System.out.println("Folder does not exist: " + dirPath5);
+                }
+                break;
 
         }
-   }
+    }
 }
